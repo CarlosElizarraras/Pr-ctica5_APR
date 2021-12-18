4f0f9a8594e062ff18d0da03f4ecf98f4f1a05d9
CarlosElizarraras <85265292+CarlosElizarraras@users.noreply.github.com>
2021-12-17 20:09:39 -0600

    Add files via upload

---
+++ Cliente.py
@@ -0,0 +1,66 @@
+import xmlrpc.client
+#crear un cliente del servidor
+def menu(case,Cliente):
+    if case==1:
+        x=str(input("Ingrese nombre o ruta (v.g: Carpeta1/hola.txt) del archivo a crear: "))
+        print(Cliente.CREATE(x))
+    elif case==2:
+        x=str(input("Ingrese nombre o ruta (v.g: Carpeta1/hola.txt): del archivo a leer: "))
+        content=Cliente.READ(x)
+        print(content)
+
+    elif case==3:
+        x=str(input("Ingrese nombre o ruta (v.g: Carpeta1/hola.txt) del archivo donde desea escribir: "))
+        mode = "a"
+        content = str(input("Escribe el contenido del archivo:\n"))
+        overwrite=int(input("¿Sobrescribir archivo?\n1.Si\t2.No\n"))
+        if overwrite==1:
+            mode="w"
+        resp=Cliente.WRITE(x,content,mode)
+        print(resp)
+    elif case == 4:
+        x=str(input("Ingrese nombre o ruta ORIGINAL del archivo (v.g: hola.txt): "))
+        y =str(input("Ingrese nombre o ruta NUEVO del archivo (v.g: aloh.txt): "))
+        resp=Cliente.RENAME(x,y)
+    elif case == 5:
+        x = str(input("Ingrese nombre o ruta (v.g: Carpeta1/hola.txt) del archivo a eliminar: "))
+        resp = Cliente.REMOVE(x)
+        print(resp)
+    elif case == 6:
+        x = str(input("Ruta del directorio (v.g. Practica5/Nombre_1/Nombre_2/.../)\n"))
+        resp = Cliente.MKDIR(x)
+        print(resp)
+    elif case == 7:
+        x = str(input("Ruta del directorio (v.g. Practica5/Nombre_1/Nombre_2/.../)\n"))
+        resp = Cliente.RMDIR(x)
+        print(resp)
+    elif case == 8:
+        x=int(input("1.Raiz\t2.Especifico"))
+        if x==1:
+            lista = Cliente.READDIR()
+            for files in lista:
+                print("\t"+files)
+        else:
+            z= str(input("Ruta del directorio (v.g. Practica5/Nombre_1/Nombre_2/.../)\n"))
+            lista = Cliente.READDIR2(z)
+            for files in lista:
+                print("\t" + files)
+    else:
+        print("Opcion no valida\n")
+def verMenu(Cliente):
+    seguir=True
+    while (seguir):
+        print("Eliga una operacion:")
+        print("1.Create\n2.Read\n3.Write\n4.Rename\n5.Remove\n6.Crear Directorio\n7.Borrar directorio\n8.Ver directorio")
+        case=int(input("Opcion: "))
+        menu(case,Cliente)
+        print("Continuar?")
+        cont=int(input("1.Si\t2.No\n"))
+        if cont==1:
+            seguir=True
+        else:
+            print()
+            seguir= False
+#The verbose option gives you debugging information useful for working out where communication errors might be happening.
+with xmlrpc.client.ServerProxy('http://192.168.0.103:7000') as Cliente:
+    verMenu(Cliente)
---
+++ Servidor.py
@@ -0,0 +1,96 @@
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+import os
+import shutil
+
+
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+with SimpleXMLRPCServer(('192.168.0.103', 7000),requestHandler=RequestHandler) as Server:
+    Server.register_introspection_functions()
+
+    def CREATE(name):
+        try:
+            with open(name,"x") as file:
+                return name + " Creado correctamente"
+                print()
+        except FileExistsError:
+            return "¡¡Archivo existente!!"
+        except FileNotFoundError:
+            return "¡¡Ruta no existente!!"
+    Server.register_function(CREATE)
+
+    def READ(name):
+        try:
+            with open(name, "r") as file:
+                return file.read()
+                print()
+        except FileNotFoundError:
+            return "¡¡Archivo/Ruta No existente!!"
+    Server.register_function(READ)
+    def WRITE(name,content,mode):
+        try:
+           with open(name, mode) as file:
+             file.write("\n"+content)
+             print()
+           return "Escritura completada!!"
+        except FileNotFoundError:
+            return "¡¡Archivo/Ruta No existente!!"
+    Server.register_function(WRITE)
+    def RENAME(name1,name2):
+        try:
+            os.rename(name1,name2)
+            return "Renombrado completo"
+        except FileNotFoundError:
+            return "¡¡Archivo/Ruta No existente!!"
+    Server.register_function(RENAME)
+
+    def REMOVE(name):
+        try:
+            os.remove(name)
+            return "Borrado completo"
+        except FileNotFoundError:
+            return "¡¡Archivo/Ruta No existente!!"
+    Server.register_function(REMOVE)
+
+    def MKDIR(ruta):
+        try:
+            os.makedirs(ruta)
+            return "Directorio creado"
+        except FileNotFoundError:
+            return "¡¡Ruta Invalida!!"
+        except FileExistsError:
+            return "¡¡Ruta existente!!"
+    Server.register_function(MKDIR)
+
+    def RMDIR(ruta):
+        try:
+            shutil.rmtree(ruta)
+            return "Directorio borrado"
+        except FileNotFoundError:
+            return "¡¡Ruta Invalida!!"
+        except FileExistsError:
+            return "¡¡Ruta existente!!"
+    Server.register_function(RMDIR)
+
+    def READDIR():
+        try:
+            lista=os.listdir()
+            return lista
+        except FileNotFoundError:
+            return "¡¡Ruta Invalida!!"
+    Server.register_function(READDIR)
+    def READDIR2(ruta):
+        try:
+            lista=os.listdir(ruta)
+            return lista
+        except FileNotFoundError:
+            return "¡¡Ruta Invalida!!"
+    Server.register_function(READDIR2)
+
+
+
+    print("Servidor iniciado")
+    Server.serve_forever()
+
